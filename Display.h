9ec55c174ad4a779af565428ced14d97317a7548
simonh88 <simon.hajek2@etu.univ-lorraine.fr>
2015-10-08 17:11:05 +0200

    adding tileset (example)

--- Display.h
+++ Display.h
@@ -6,7 +6,7 @@
 #include <SFML/System.hpp>
 
 
-class Display
+class Display : public sf::Drawable, public sf::Transformable
 {
     public:
         Display();
@@ -16,10 +16,25 @@ class Display
         //Just for test
         void printCheckerboard();
         //idem
-        void tailleComposants(sf:: RenderWindow *window);
+        void tailleComposants(sf::RenderWindow *window);
+
+
+        void loadTileMap(const std::string& tileset, sf::Vector2u tileSize,
+        std::vector<std::vector<int> > dataMap, unsigned int width,
+        unsigned int height);
 
     private:
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+    {
+    // on applique la transformation
+    states.transform *= getTransform();
+    states.texture = &tileset;
+    target.draw(vertices, states);
+    }
+
     std::vector<std::vector<int> > realCheckerboard;
+    sf::VertexArray vertices;
+    sf::Texture tileset;
 
 };
 
